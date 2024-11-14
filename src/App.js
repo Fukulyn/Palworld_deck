7b5096e3e6f3abfd6a6d2a4769e56010f1fb8b9a
TKU-AI-411630188 <112963598+Alison411630188@users.noreply.github.com>
2024-11-14 13:39:40 +0800

    1315456

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ function App() {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   const characters = [
-    { id: 0, name: '帕魯', description: (<>棉悠悠<br /> 無屬性<br /> 工作適應性:手作 搬運 牧場 </>), image: 'https://patchwiki.biligame.com/images/palworld/d/dc/5ttetm0toqg3foaw3josy874pajeqyi.png' },
-    { id: 1, name: '龍', description: 'A wise mage with powerful spells.', image: 'https://images-ext-1.discordapp.net/external/dbXGhyqlO5pySqpuHrQKpSjHXhdeR4hmQogv-lqM1aw/https/patchwiki.biligame.com/images/palworld/d/d1/09ag6ma7ej9g1lh175ehcksldajmpmx.png?format=webp&quality=lossless&width=640&height=640' },
-    { id: 2, name: 'A', description: 'A swift archer with deadly accuracy.', image: ''},
-    { id: 3, name: 'B', description: 'A swift archer with deadly accuracy.', image: ''},
-    { id: 4, name: 'C', description: 'A swift archer with deadly accuracy.', image: ''},
+    { id: 0, name: '搗蛋貓', description: (<>搗蛋貓<br /> 屬性:無屬性<br /> 工作適應性:手作、搬運、採礦、採集 </>), image: 'https://patchwiki.biligame.com/images/palworld/d/dc/5ttetm0toqg3foaw3josy874pajeqyi.png' },
+    { id: 1, name: '棉悠悠', description: (<>棉悠悠<br /> 屬性:無屬性<br /> 工作適應性:手作、搬運、牧場 </>), image: 'https://patchwiki.biligame.com/images/palworld/d/d1/09ag6ma7ej9g1lh175ehcksldajmpmx.png '},
+    { id: 2, name: '皮皮雞', description: (<>皮皮雞<br /> 屬性:無屬性<br /> 工作適應性:牧場、採集</>), image: 'https://patchwiki.biligame.com/images/palworld/e/e9/ah1x6mdomks0x6jg8wmsh6gysivxrmi.png'},
+    { id: 3, name: '新葉猿', description: (<>新葉猿<br /> 屬性:無屬性<br /> 工作適應性:伐木、手作、搬運、播種、採集 </>), image: 'https://patchwiki.biligame.com/images/palworld/5/5d/piosi94mfck7x5inrradc5fhjnp6nhh.png'},
+    { id: 4, name: 'C', description: 'A swift archer with deadly accuracy.', image: 'https://patchwiki.biligame.com/images/palworld/7/77/shcqv7vpc81890bi4r8lmt7irjhllhw.png'},
     { id: 5, name: 'D', description: 'A swift archer with deadly accuracy.', image: ''},
     { id: 6, name: 'E', description: 'A swift archer with deadly accuracy.', image: ''},
     { id: 7, name: 'F', description: 'A swift archer with deadly accuracy.', image: ''},
