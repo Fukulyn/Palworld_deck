c0b8c5356c171b9004dad903f833097649538ff4
Fukulyn <a098838628@gmail.com>
2024-11-14 13:54:26 +0800

    框架修改

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,7 +3,7 @@ import './CharacterCard.css';
 
 function CharacterCard({ character, onClick }) {
   return (
-    <div className="character-card card m-2" onClick={onClick} style={{ width: '150px', cursor: 'pointer' }}>
+    <div className="character-card card m-2" onClick={onClick} style={{ width: '160px', cursor: 'pointer' }}>
       <img src={character.image} alt={character.name} className="card-img-top" />
       <div className="card-body">
         <h5 className="card-title">{character.name}</h5>
